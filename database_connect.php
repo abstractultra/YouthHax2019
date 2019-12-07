7b1ea9b7a8f0087bb27186b90bc674ceb24ee4ee
AbstractUltra <dragonhe36@gmail.com>
2019-12-07 14:53:04 -0500

    i

---
+++ database_connect.php
@@ -0,0 +1,2 @@
+<?php
+$conn = new mysqli($server_name, $db_username, $db_password, $database_name);
--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 $data = file_get_contents("https://www.rescuetime.com/anapi/data?key=B63SD5uLHof2HskzdelSGP5Tp2d5QyGdE8J0ELSQ");
 $stuff = json_decode($data, true);
-for ($i : stuff) {
-    
+foreach ($stuff as $name => $value) {
+
 }
