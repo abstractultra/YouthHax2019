2d7ad3c060debd40a4e38ba65e288cd50ad50e1d
AbstractUltra <dragonhe36@gmail.com>
2019-12-07 15:42:41 -0500

    i

--- main.php
+++ main.php
@@ -2,5 +2,11 @@
 $data = file_get_contents("https://www.rescuetime.com/anapi/data?key=B63SD5uLHof2HskzdelSGP5Tp2d5QyGdE8J0ELSQ");
 $stuff = json_decode($data, true);
 foreach ($stuff as $name => $value) {
-
+    if ($name == "rows") {
+        foreach($idk as $names => $text) {
+            if ($text == "youtube.com") {
+                g
+            }
+        }
+    }
 }
